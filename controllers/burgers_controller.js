2235bce1309862f2da2c4a15f4ebfa7ea8186877
monsterreh2b <brant.han@gmail.com>
2017-07-04 22:36:05 -0700

    update config and controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,6 +5,12 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var db = require("../models");
 
+// Extracts the sequelize connection from the models object
+var sequelizeConnection = db.sequelize;
+
+// Sync the tables
+sequelizeConnection.sync();
+
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
     //     db.burger.all(function(data) {
